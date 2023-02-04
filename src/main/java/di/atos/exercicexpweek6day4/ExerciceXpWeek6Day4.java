c6ec1543f354ebebcc2fbe79391f53fa2a13ecc2
macbook <wkoua@oxylead.net>
2023-02-04 06:24:05 +0000

    commit

---
+++ src/main/java/di/atos/exercicexpweek6day4/ExerciceXpWeek6Day4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.atos.exercicexpweek6day4;
+
+import di.atos.exercicexpweek6day4.exercice1.Student;
+
+public class ExerciceXpWeek6Day4 {
+
+    public static void main(String[] args) {
+
+        //tout les programmes devraient etre lancé dans la methode main ici 
+        // Exercice 1
+        Student student = new Student("John", 2);
+        // Exercice 2
+    }
+}
